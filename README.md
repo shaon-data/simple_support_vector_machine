e138dda97373bbf0b22ce19231a94a2d6b388b35
Shaon Majumder <shaon.fdr@robist.com>
2018-06-30 13:37:54 +0600

    1st commit via 1 step

---
+++ README.md
@@ -0,0 +1,4 @@
+# simple_support_vector_machine 
+## Download 
+For downloading use 
+       `git clone https://github.com/ShaonMajumder/simple_support_vector_machine.git` 
---
+++ svm.py
@@ -0,0 +1,38 @@
+## commit
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
+style.use('ggplot')
+
+plus_class = 1
+minus_class = -1
+
+## for avoiding retraining we are making class for object creation
+class svm:
+    def __init__(self, visualize=True):
+        self.visualize = visualize
+        self.colors = {plus_class:'r',minus_class:'b'}
+        
+        if self.visualize:
+            self.fig = plt.figure()
+            self.ax = self.fig.add_subplot(1,1,1)
+
+        ## train
+        def fit(self,data):
+            pass
+        def predict(self,features):
+            # sign(x.w+b)
+            classify = np.sign( np.dot(np.array(features),self.w) + self.b )
+            return classify
+            
+data_dict = {
+              plus_class:np.array([[1,7],
+                                   [2,8],
+                                   [3,8]
+                                  ]),
+              
+              minus_class:np.array([[5,1],
+                                    [6,-1],
+                                    [7,3]
+                                  ])
+            }
